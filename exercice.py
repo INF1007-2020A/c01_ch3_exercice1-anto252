2c9a95c7164e40557e30a0867a329342f6de2f55
anto252 <antoinecc@hotmail.ca>
2020-09-14 16:56:15 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -3,12 +3,11 @@
 
 import math
 def square_root(number: int) -> float:
-    # TODO completer la fonction
     return number ** 0.5
 
 
 def square(number: int) -> int:
-    # TODO completer la fonction
+   
     return number ** 2
 
 def main() -> None:
